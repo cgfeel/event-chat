258e78bf5e8b881c5e8127ee5e960a0103fbdcdc
levi <cgfeel@gmail.com>
2026-06-14 06:47:08 +0800

    fix: provider observerRPC

--- packages/rpc/src/index.ts
+++ packages/rpc/src/index.ts
@@ -1,4 +1,4 @@
 export * from './createCtx'
 export type { MessageItem, Transport } from './fields'
 export type { MessageInfoType as MessageInfo } from './core/RPCAction'
-export type { RPCIns } from './core/observerRPC'
+export { type RPCIns, observerRPC } from './core/observerRPC'
